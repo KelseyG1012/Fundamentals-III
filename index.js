d2496d1683819c14c3fbc811d50d836f81726a6a
KelseyG1012 <kelsey370@gmail.com>
2024-10-08 10:15:12 -0700

    Update funtionality

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
 
 
 //add event listener for form
-document.getElementById("submitForm").addEventListener("submit", addTodoItem(document.getElementById("submitContent").value));
+
+document.getElementById("addItemBtn").addEventListener("click", addTodoItem);
 
 /**
  *Takes in a string, content and appends a TODO to the listContainer.
  */
-function addTodoItem(content){
+function addTodoItem(){
+    let content = document.getElementById("contentText").value;
     console.log(`Starting addTodoItem function with content: ${content}`);
+    let orderedList = document.getElementById("todoList");
+
+    //create all the elements needed for a todo item
+    const newListItem = document.createElement('li');
+
+    //add the relevant fields for each todo item property
+    //newItem.textContent = content;
+
+    //paragraph
+    const newP = document.createElement('p');
+    newP.textContent = content;
+
+
+    //checkbox
+    const newCheckBox = document.createElement('input');
+    newCheckBox.classList.add("checkbox");
+    newCheckBox.type = "checkbox";
+    newCheckBox.value = "check";
+
+    //button
+    const newRemoveBtn = document.createElement('button');
+    newRemoveBtn.classList.add("removeBtn");
+    newRemoveBtn.type = "button";
+    newRemoveBtn.textContent = "remove";
+
+    //add all todo list elements to the list item
+    newListItem.appendChild(newP);
+    newListItem.appendChild(newCheckBox);
+    newListItem.appendChild(newRemoveBtn);
+
+
+    //add list item to the todo list
+    orderedList.appendChild(newListItem);
+}
+
+
+function removeTodoItem(){
+
 }
 
